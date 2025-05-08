61c12f3575ad4570f5f476e04fd921dd2f0529ad
Miriam564w <ok.angulo.miriam@leadps.org>
2025-05-08 10:01:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿let user = "";
+let choices = ["rock", "paper", "scissors"];
+
+
+function select(selection) {
+  user = selection;
+  let computer = choices[Math.floor(Math.random() * choices.length)];
+  let result = "";
+
+
+  if (computer === user) {
+    result = "It's a tie!";
+  } else if (
+    (user === "rock" && computer === "paper") ||
+    (user === "paper" && computer === "scissors") ||
+    (user === "scissors" && computer === "rock")
+  ) {
+    result = `You lose! ${computer} beats ${user}`;
+  } else {
+    result = `You win! 🎉 ${user} beats ${computer}`;
+  }
+
+
+  document.getElementById("result").innerHTML = result;
+}
